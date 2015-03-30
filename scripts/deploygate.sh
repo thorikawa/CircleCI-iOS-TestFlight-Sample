ce5db2759c875b447c5d85c897c431bad458f1a9
mollifier <mollifier@gmail.com>
2015-03-30 18:35:48 +0900

    fix

--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-#PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_UUID.mobileprovision"
 RELEASE_DATE=$(date '+%Y-%m-%d %H:%M:%S')
 ARCHIVE_PATH="$PWD/build.xcarchive"
 APP_DIR="$ARCHIVE_PATH/Products/Applications"
-DSYM_DIR="$ARCHIVE_PATH/dSYMs"
 
 echo "********************"
 echo "*     Archive      *"
@@ -16,22 +14,7 @@ echo "*     Signing      *"
 echo "********************"
 xcrun -sdk iphoneos PackageApplication -v  "$APP_DIR/$APPNAME.app" -o "$APP_DIR/$APPNAME.ipa"
 
-#RELEASE_NOTES="Build: ${CIRCLE_BUILD_NUM} Uploaded: $RELEASE_DATE"
-RELEASE_NOTES="$RELEASE_DATE"
-curl -F file="@$APP_DIR/$APPNAME.ipa"  -F "token=${DEPLOYGATE_API_TOKEN}" -F message="$RELEASE_NOTES" https://deploygate.com/api/users/${DEPLOYGATE_USER_NAME}/apps
-
-
-
-#zip -r -9 "$DSYM_DIR/$APPNAME.app.dSYM.zip" "$DSYM_DIR/$APPNAME.app.dSYM"
-
-# echo "********************"
-# echo "*    Uploading     *"
-# echo "********************"
-# curl http://testflightapp.com/api/builds.json \
-#   
-#   -F dsym="@$DSYM_DIR/$APPNAME.app.dSYM.zip" \
-#   -F api_token="$API_TOKEN" \
-#   -F team_token="$TEAM_TOKEN" \
-#   -F distribution_lists="$TESTFLIGHT_DIST_LIST" \
-#   -F notes="$RELEASE_NOTES" -v
+# deploy
+RELEASE_NOTES="Build: ${CIRCLE_BUILD_NUM}, Uploaded: $RELEASE_DATE"
+curl -F file="@$APP_DIR/$APPNAME.ipa"  -F "token=${DEPLOYGATE_API_TOKEN}" -F message="$RELEASE_NOTES" "https://deploygate.com/api/users/${DEPLOYGATE_USER_NAME}/apps"
 
